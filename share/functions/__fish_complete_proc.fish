5edcd6cde6e8fda2dee21a3891735542b5509b0b
ridiculousfish <corydoras@ridiculousfish.com>
2013-01-15 11:32:30 -0800

    Attempt to fix proc and pid completion on OS X

--- share/functions/__fish_complete_proc.fish
+++ share/functions/__fish_complete_proc.fish
@@ -1,4 +1,6 @@
 function __fish_complete_proc --description 'Complete by list of running processes'
-	ps -A --no-headers --format comm | sort -u
-
+	# 'tail -n +2' deletes the first line, which contains the headers
+	# sed removes the trailing whitespace and a leading dash (which means login shell)
+	# Don't use ucomm, because it truncates names on OS X
+	ps axc -o comm | tail -n +2 | sed -e 's/^-//g' -e 's/ *$//g' | sort -u
 end
